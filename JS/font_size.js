bce186916dd8747ba4f1bc69c65219c0737a9e87
Head0nF1re <77078775+Head0nF1re@users.noreply.github.com>
2021-04-29 05:02:53 +0100

    Few changes in index.html and css
    * Font-Size
    * Style buttons change font-size
    * Style links
    * Add JS to allow dark mode -> Needs to be updated

--- JS/font_size.js
+++ JS/font_size.js
@@ -16,3 +16,45 @@ $('#diminui-texto').click(function() {
     if (curSize >= 12)
         $('#content').css('font-size', curSize);
 });
+
+
+//Change Theme function
+
+var flag=false;
+function changeColor(bgColor) {
+
+
+	var otherColor = document.getElementsByClassName("otherColor");
+	var spanColor2 = document.getElementById("spanColor2");
+	var color_2 = document.getElementById("color_2");
+
+	if (flag == false) {
+
+		flag=true;
+
+		document.body.style.background = bgColor;
+		spanColor2.style.background = bgColor;
+		color_2.style.background = '#292929';
+
+		for (var i = 0; i < otherColor.length; i++) {
+
+	   		otherColor.item(i).style.color = 'white';
+		}
+
+	}
+	else{
+
+		document.body.style.backgroundColor = 'white';
+		spanColor2.style.background = 'white';
+		color_2.style.background = '#0f354e';
+
+		for (var i = 0; i < otherColor.length; i++) {
+
+	   		otherColor.item(i).style.color = '#252525';
+		}
+
+		flag=false;
+	}
+
+};
+
